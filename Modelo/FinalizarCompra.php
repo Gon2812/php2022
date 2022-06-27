3ccdc73247fb2198c7c8ff32b1fec004b8c633bd
narenbiardo <narenbiardo@gmail.com>
2022-06-27 19:48:38 -0300

    se agrega control de la cantidad de mercaderia al agregar al carrito y se elimina el stock al comprar

--- Modelo/FinalizarCompra.php
+++ Modelo/FinalizarCompra.php
@@ -3,11 +3,23 @@ session_start();
 include "../Persistencia/Conexion.php";
 $idSesion =$_SESSION['id'];
 
+$carrito = mysqli_query($conexion, "SELECT * FROM carrito WHERE id_cliente='$idSesion'");
+
+while($c=mysqli_fetch_assoc($carrito)){
+    $idProductoEliminar = $c["id_producto"];
+    $stockProductoActual = mysqli_fetch_assoc(mysqli_query($conexion, "SELECT * FROM mercaderia WHERE id=$idProductoEliminar"))["stock"];
+    $stockProductoEliminar = $c["cantidad"];
+    $nuevoStockProducto = (int)$stockProductoActual - (int)$stockProductoEliminar;
+
+    $cambiarStock = "UPDATE mercaderia SET stock = $nuevoStockProducto WHERE id=$idProductoEliminar";
+    $resultadoCambioStock = mysqli_query($conexion, $cambiarStock);
+}mysqli_free_result($carrito);
+
 $borrar = "Delete from carrito where id_cliente='$idSesion'";
 
 $resultado = mysqli_query($conexion, $borrar);
 
-if($resultado){
+if($resultadoCambioStock && $resultado){
     echo "<script>alert('Se ha relizado la compra con éxito'); window.location='../Vista/Catalogo.php'</script>";
 }
 else{
--- Modelo/agregarACarrito.php
+++ Modelo/agregarACarrito.php
@@ -27,20 +27,32 @@ function test_input($data) {
         $cantidad = test_input($_POST["cantidad"]);
         $total = $price*$cantidad;
         $idSesion =$_SESSION['id'];
+        $stockMercaderia = mysqli_fetch_assoc(mysqli_query($conexion, "SELECT * FROM mercaderia WHERE id=$id"))["stock"];
+        $stockMercaderiaEnCarrito = mysqli_fetch_assoc(mysqli_query($conexion, "SELECT * FROM carrito WHERE id_producto=$id"))["cantidad"];
 
-        $insertar = "INSERT INTO carrito(id_cliente, cantidad, id_producto, precio, nombreProducto,total)
-        VALUES('$idSesion', '$cantidad', '$id', '$price', '$productName','$total')";
+        if((int)$cantidad > 0 && ((int)$stockMercaderia - (int)$stockMercaderiaEnCarrito) >= (int)$cantidad){
 
-        $resultado = mysqli_query($conexion, $insertar);
+            $insertar = "INSERT INTO carrito(id_cliente, cantidad, id_producto, precio, nombreProducto,total)
+            VALUES('$idSesion', '$cantidad', '$id', '$price', '$productName','$total')";
 
-        if($resultado){
-            echo "<script>alert('Se ha agregado al carrito con éxito'); window.location='../Vista/Catalogo.php'</script>";
+            $resultado = mysqli_query($conexion, $insertar);
+
+            if($resultado){
+                echo "<script>alert('Se ha agregado al carrito con éxito'); window.location='../Vista/Catalogo.php'</script>";
+            }
+            else{
+                echo"<script>alert('No se pudo agregar al carrito');window,history.go(-1);</script>";
+            }
+            mysqli_close($conexion);
         }
         else{
-            echo"<script>alert('No se pudo agregar al carrito');window,history.go(-1);</script>";
+            if((int)$cantidad < 1){
+                echo"<script>alert('La cantidad del producto seleccionado debe de ser mayor a 0');window,history.go(-1);</script>";
+            }
+            else{
+                echo"<script>alert('No se pudo agregar al carrito por falta de stock');window,history.go(-1);</script>";
+            }
         }
-        mysqli_close($conexion);
-    
     }
 
 ?>
