f7bf137c807d1ca2705d4a5e3918574339679749
Gonzalo Cánepa <69828698+Gon2812@users.noreply.github.com>
2022-06-28 16:40:44 -0300

    Add files via upload

--- Modelo/HabilitarUsuario.php
+++ Modelo/HabilitarUsuario.php
@@ -5,27 +5,28 @@ $id = $_GET['id'];
 $insertar='';
 
 if($_SERVER["REQUEST_METHOD"] == "GET"){
-    $dato = "SELECT habilitado FROM cliente WHERE id = '$id'";
+    $dato = "SELECT tipo, habilitado FROM cliente WHERE id = '$id'";
     $bool = mysqli_query($conexion, $dato);
     while($fila=mysqli_fetch_assoc($bool)){
-        
-        echo $fila['habilitado'];
-        if($fila['habilitado'] == true){
-            echo "Entré al if";
-            $insertar = "UPDATE `cliente` SET `habilitado` = '0' WHERE `id`='$id'";
-        }
-        else{
-            echo "Entré al else $id";
-            $insertar = "UPDATE `cliente` SET `habilitado` = '1' WHERE `id`='$id'";
+        if($fila['tipo'] == "local"){
+            if($fila['habilitado'] == true){
+                echo "Entré al if";
+                $insertar = "UPDATE `cliente` SET `habilitado` = '0' WHERE `id`='$id'";
+            }
+            else{
+                echo "Entré al else $id";
+                $insertar = "UPDATE `cliente` SET `habilitado` = '1' WHERE `id`='$id'";
+            }
+            $resultado = mysqli_query($conexion, $insertar);
         }
     }
-    $resultado = mysqli_query($conexion, $insertar);
+    
 
     mysqli_close($conexion);
     if($resultado){
         echo "<script>alert('Modificado con éxito.'); window.location='/ObligatorioPHP/Vista/MenuPrincipalAdmin.php'</script>";
     }
     else{
-        echo"<script>alert('No se pudo registrar');window,history.go(-1);</script>";
+        echo"<script>alert('No es buena idea deshabilitar a un admin...');window,history.go(-1);</script>";
     }
 }
